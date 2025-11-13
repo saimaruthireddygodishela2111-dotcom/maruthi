bf9fba4eae0c64c887bfcaf4937703fdf44e7378
Sai Maruthi <saimaruthireddygodishela2111@gmail.com>
2025-11-13 19:16:54 +0530

    first commit

--- createinstance.tf
+++ createinstance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "MyFirstInstance" {
     ami            = "ami-05803413c51f242b7"
     instance_type  = "t2.micro"
 
-    tags{
+    tags = {
         Name = "threedemoinstance"
     }
 
