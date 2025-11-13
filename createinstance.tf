d7a62d67814937678759ad1cd439912fc09a3dd1
Sai Maruthi <saimaruthireddygodishela2111@gmail.com>
2025-11-13 19:03:00 +0530

    updated code

--- createinstance.tf
+++ createinstance.tf
@@ -1,7 +1,12 @@
 
 resource "aws_instance" "MyFirstInstance" {
+    count          = 3
     ami            = "ami-05803413c51f242b7"
     instance_type  = "t2.micro"
+
+    tags{
+        Name = "threedemoinstance"
+    }
 
 }
 
