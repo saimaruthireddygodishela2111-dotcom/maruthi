65a22f2fcb3e1326983de070456c6e39503cc60d
Sai Maruthi <saimaruthireddygodishela2111@gmail.com>
2025-11-13 18:45:15 +0530

    your update message

--- createinstance.tf
+++ createinstance.tf
@@ -1,9 +1,3 @@
-provider "aws"{
-
-    access_key = "AKIAVZYJ3S4OQZUHH7NK"
-    secret_key = "SECRET_KEY_HERE"
-    region     = "us-east-2"
-}
 
 resource "aws_instance" "MyFirstInstance" {
     ami            = "ami-05803413c51f242b7"
